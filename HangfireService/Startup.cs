c62e9d897cfb570285ea6655fc24ca1157cba4bf
Nikita Temnikov <nistoc@gmail.com>
2017-07-05 10:26:54 +0300

    .NET Framework version was decreased; Hangfire package was added.

---
+++ HangfireService/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Hangfire;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HangfireService.Startup))]
+
+namespace HangfireService
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage(@"Server=(localdb)\mssqllocaldb; Database=testHangFire01; trusted_connection=false;user=HangFire;password=pass@123");
+
+            //app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
