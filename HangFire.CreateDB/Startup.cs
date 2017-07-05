690ef6fc617441e5128981d21a56c8851436d6e8
Nikita Temnikov <nistoc@gmail.com>
2017-07-05 09:45:11 +0300

    remove all projects

--- HangFire.CreateDB/Startup.cs
+++ HangFire.CreateDB/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -29,6 +26,7 @@ namespace HangFire.CreateDB
         {
             // Add framework services.
             services.AddMvc();
+            services.AddHangfire(x => x.UseSqlServerStorage("<connection string>"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
